bd3cbf8f9e7856a55b201215756a1b27d94bacac
Ueyama Satoshi <gyuque@gmail.com>
2011-05-08 02:40:21 +0900

    drawing board buttons

--- js/oqunomaker.js
+++ js/oqunomaker.js
@@ -23,6 +23,8 @@ if (!window.gyuque) { window.gyuque ={}; }
 		this.canvasSize = size;
 		this.containerElement = containerElement;
 		this.buildView(containerElement, size);
+		this.signStrokes = null;
+		this.strokeTransform = new pkg.StrokeTransform();
 
 		this.params = {
 			phase_body_1: 0,
@@ -54,10 +56,24 @@ if (!window.gyuque) { window.gyuque ={}; }
 		this.radii_face = new Array(FourierParams.face.R.length);
 		for (i = 0;i < this.radii_face.length;i++){ this.radii_face[i]=1; }
 
+		this.board = new pkg.DrawBoard(size, size >> 1, this);
+		this.containerElement.appendChild(this.board.outerElement());
+		this.board.fit();
+
 		this.resetParams();
 	};
 
 	pkg.OqunoMaker.prototype = {
+		onSignButton: function() {
+			this.board.clear();
+			this.board.show();
+		},
+
+		onDrawBoardOK: function(strokes) {
+			this.signStrokes = strokes.slice();
+			this.update();
+		},
+
 		calcBasePosition: function(p) {
 			var dx = FourierParams.face.center.x - FourierParams.body.center.x;
 			var dy = FourierParams.face.center.y - FourierParams.body.center.y;
@@ -71,7 +87,9 @@ if (!window.gyuque) { window.gyuque ={}; }
 		},
 
 		buildView: function(container) {
+			var _this = this;
 			container.innerHTML = "";
+			container.style.position = "relative";
 
 			var tbl = $H('table');
 			var row1 = $H('tr');
@@ -82,12 +100,29 @@ if (!window.gyuque) { window.gyuque ={}; }
 			row1.appendChild(col1);
 			row1.appendChild(col2);
 
+			this.offscreenCanvas = $H("canvas");
+			this.offscreenCanvas.width = this.canvasSize;
+			this.offscreenCanvas.height = this.canvasSize;
+
 			this.canvas = $H("canvas", "oqunomaker-main-canvas");
 			this.canvas.width = this.canvasSize;
 			this.canvas.height = this.canvasSize;
+			this.canvas.style.display = "block";
 			col1.appendChild(this.canvas);
 
-			var _this = this;
+			// sign button
+			var sbtn_outer = $H("div", "oqunomaker-sign-button");
+			var sbtn_inner = $H("input");
+			sbtn_inner.type = "image";
+			sbtn_inner.src = "images/write-icon.png";
+			sbtn_inner.title = "サインを入れる";
+			add_css(".oqunomaker-sign-button input:active{opacity:0.5;} .oqunomaker-sign-button{width:24px; height:24px; margin-left: "+(this.canvasSize-32)+"px; margin-top: -32px; background: url(images/button-shadow.png) no-repeat top left}");
+			sbtn_outer.appendChild(sbtn_inner);
+			col1.appendChild(sbtn_outer);
+			$(sbtn_inner).click(function(){
+				_this.onSignButton();
+			});
+
 			var update_closure = function(){ _this.update(); };
 			var opts = {
 				change: update_closure,
@@ -133,6 +168,7 @@ if (!window.gyuque) { window.gyuque ={}; }
 			this.all_sliders = [slider1, slider2, slider3, slider4, slider5, slider6, slider7, slider10];
 
 			this.g = this.canvas.getContext('2d');
+			this.g2 = this.offscreenCanvas.getContext('2d');
 			this.gradHighlight = this.g.createLinearGradient(0, (this.canvasSize >> 1) - FourierParams.body.R[0] * MasterScale, 0, (this.canvasSize >> 1) + FourierParams.body.R[0] * MasterScale);
 			this.gradHighlight.addColorStop(0  , '#444');
 			this.gradHighlight.addColorStop(0.37, '#1a1a1a');
@@ -244,12 +280,15 @@ if (!window.gyuque) { window.gyuque ={}; }
 			g.fill();
 			g.restore();
 
+
 			if (this.params.highlight) {
 				if (this.params.glassy)
 					this.drawGlass(g, scale);
 
 				this.drawHighlight(g, scale);
 			}
+
+			this.drawSign(this.g, f.R, f.i, this.radii_body, this.shift_body, cx + f.center.x*scale, cy + f.center.y*scale, scale, this.params.highlight ? 0.8 : 1);
 
 			var fcx = f.center.x + Math.cos(this.params.face_angle - this.params.phase_body_2*0.3) * this.params.face_distance;
 			var fcy = f.center.y + Math.sin(this.params.face_angle - this.params.phase_body_2*0.3) * this.params.face_distance;
@@ -347,10 +386,10 @@ if (!window.gyuque) { window.gyuque ={}; }
 			g.fill();
 
 			g.beginPath();
-			g.moveTo(0,0);
-			g.lineTo(0,this.canvasSize);
-			g.lineTo(this.canvasSize,this.canvasSize);
-			g.lineTo(this.canvasSize, 0);
+			g.moveTo(-32,0);
+			g.lineTo(-32,this.canvasSize+32);
+			g.lineTo(this.canvasSize+32,this.canvasSize+32);
+			g.lineTo(this.canvasSize+32, 0);
 			this.makeOqunoPath(g, f.R, f.i, this.radii_body, this.shift_body, cx + f.center.x*scale, cy + (f.center.y-22)*scale, scale*1.1, 90);
 			g.fillStyle='#000';
 			g.shadowBlur = f.R[0] * scale * 0.4;
@@ -391,6 +430,115 @@ if (!window.gyuque) { window.gyuque ={}; }
 			g.beginPath();
 			g.arc(cx, cy, size*0.5, 0, Math.PI*2, false);
 			g.fill();
+		},
+
+		drawSign: function(g, Rs, is, rMods, iShifts, cx, cy, radius, strokeAlpha) {
+			if (!this.bottomYMap) { this.bottomYMap = new Array(this.canvasSize); }
+
+			var i;
+			var ymap = this.bottomYMap;
+			var mlen = ymap.length;
+			for (i = 0;i < mlen;i++) {
+				ymap[i] = -1;
+			}
+
+			var hm = mlen >> 1;
+			var DIVS = 100;
+			var DPI = Math.PI * 2.0;
+			for (var k = 0;k < DIVS;k++) {
+				var t = (k / (DIVS-1));
+				var a = Math.asin(t*2.0 - 1.0);
+				var rd  = this.iFT(Math.PI-a, Rs, is, rMods, iShifts);
+				var rd2 = this.iFT(Math.PI-a-0.1, Rs, is, rMods, iShifts);
+				var y = -Math.cos(Math.PI-a) * rd * radius;
+				var x  =  Math.sin(Math.PI-a) * rd * radius;
+				var x2 =  Math.sin(Math.PI-a-0.1) * rd2 * radius;
+				if (x < x2) {
+					var ax = Math.floor(x + hm);
+					if (ax >= 0 && ax < mlen) {
+						if (y > ymap[ax]) {
+							ymap[ax] = y;
+						}
+					}
+				}
+			}
+
+			var prevx = -1;
+			for (i = 0;i < (mlen-1);i++) {
+				if (ymap[i] >= 0 && ymap[i+1] < 0) {
+					prevx = i;
+				} else if (ymap[i] < 0 && ymap[i+1] >= 0) {
+					if (prevx >= 0) {
+						this.intpArray(ymap, prevx, i+1);
+					}
+				}
+			}
+
+			var dy;
+			var dyLimit = this.canvasSize >> 6;
+			var rightEnd = 0;
+			var leftEnd = mlen-1;
+			for (i = 0;i < (mlen>>2);i++) {
+				dy = ymap[i] - ymap[i+1];
+				if (dy<0){dy = -dy}
+
+				if (dy <= dyLimit && ymap[i] >= 0) {
+					leftEnd = i;
+					break;
+				}
+			}
+
+			for (i = (mlen-1);i > (mlen-(mlen>>2));i--) {
+				dy = ymap[i] - ymap[i-1];
+				if (dy<0){dy = -dy}
+
+				if (dy <= dyLimit && ymap[i] >= 0) {
+					rightEnd = i;
+					break;
+				}
+			}
+
+/*
+			console.log(leftEnd+'  '+rightEnd);
+
+			g.fillStyle = "#f00";
+			for (i = 0;i < mlen;i++) {
+				if (ymap[i] >= 0)
+					g.fillRect(i, cy + ymap[i]-2, 1, 3);
+			}
+*/
+
+			if (this.signStrokes) {
+				this.g2.clearRect(0, 0, this.canvasSize, this.canvasSize);
+
+				this.strokeTransform.leftEnd = leftEnd;
+				this.strokeTransform.cy = Math.floor(this.canvasSize * 0.4);
+				this.strokeTransform.scaleX = (rightEnd-leftEnd)/mlen;
+				this.strokeTransform.scaleY = 1.1;
+				this.strokeTransform.yTable = ymap;
+				for (i = 0;i < this.signStrokes.length;i++) {
+					this.board.renderStroke(this.g2, this.signStrokes[i], "#fff", this.strokeTransform);
+				}
+
+				g.save();
+				g.globalAlpha = strokeAlpha;
+				var ptn = g.createPattern(this.offscreenCanvas, "repeat");
+				g.fillStyle = ptn;
+				g.fillRect(0, 0, this.canvasSize, this.canvasSize);
+				g.restore();
+			}
+		},
+
+		intpArray: function(a, p1, p2) {
+			var len = (p2-p1) - 1;
+			if (len < 1) return;
+
+			var v1 = a[p1];
+			var v2 = a[p2];
+			for (var i = 0;i < len;i++) {
+				var t = (i+1) / (len+1);
+				a[p1 + i + 1] = v1 * (1.0 - t) + v2 * t;
+			}
 		},
 
 		iFT: function(a, Rs, is, rMods, iShifts) {
@@ -475,6 +623,294 @@ if (!window.gyuque) { window.gyuque ={}; }
 		}
 	};
 
+	pkg.StrokeTransform = function() {
+		this.scaleX = 1;
+		this.scaleY = 1;
+		this.leftEnd = 0;
+		this.cy = 0;
+		this.yTable = null;
+	};
+
+	pkg.StrokeTransform.prototype = {
+		setYTable: function() {
+		},
+
+		transformX: function(x) {
+			return x  * this.scaleX + this.leftEnd;
+		},
+
+		transformY: function(x, y, h) {
+			var t = y/h * 0.5 + 0.5;
+			return this.cy + (this.yTable[Math.floor(this.transformX(x))] * t * t + y*(1.0-t)) * this.scaleY;
+		}
+	};
+
+	// ========================= DrawBoard ===========================
+	pkg.DrawBoard = function(w, h, lsn) {
+		this.listener = lsn;
+		this.width = w;
+		this.height = h;
+		this.lineWidth = 6.0;
+		var _this = this;
+
+		this.pageOrigin = {x:0,y:0};
+		this.outer  = $H('div', 'oqunomaker-drawboard-outer');
+		this.canvas = $H('canvas', 'oqunomaker-drawboard');
+		this.g = this.canvas.getContext('2d');
+		this.outer.appendChild(this.canvas);
+		this.outer.addEventListener('mousedown', function(e){ e.preventDefault();}, false);
+
+		this.dragging = false;
+		var cv = this.canvas;
+		cv.width = w;
+		cv.height = h;
+		cv.style.background = "#fff";
+		cv.style.border = "1px solid #000";
+		cv.style.boxShadow = "0 1px 1px #000";
+		$(cv).mousedown(function(e){ _this.onMouseDown(e); }).
+		      mousemove(function(e){ _this.onMouseMove(e); }).
+		      mouseleave(function(e){ _this.onMouseLeave(e); }).
+		      mouseup(function(e){ _this.onMouseUp(e); });
+
+		var os = this.outer.style;
+		os.width = "100%";
+		os.position = "absolute";
+		os.top = "0";
+		os.left = "0";
+		os.zIndex = "2";
+		os.textAlign = "center";
+		os.backgroundColor="rgba(0,0,0,0.4)";
+		
+		os.display = "none";
+
+		var buttonBox = $H('div', 'oqunomaker-drawboard-buttons');
+		var btnOK = $H('button');
+		$(btnOK).text("OK").click(function(){
+			_this.close();
+			if (_this.listener) {
+				_this.listener.onDrawBoardOK(_this.strokes);
+			}
+		}).css("background-color", "#3e5");
+
+		var btnClear = $H('button');
+		$(btnClear).text("消す").click(function(){
+			_this.clear();
+		});
+
+		var btnCancel = $H('button');
+		$(btnCancel).text("キャンセル").click(function(){
+			_this.close();
+		}).css("margin", "9px");
+
+		buttonBox.appendChild(btnClear);
+		buttonBox.appendChild(btnCancel);
+		buttonBox.appendChild(btnOK);
+		this.outer.appendChild(buttonBox);
+
+		this.strokes = [];
+		this.currentStroke = null;
+		this.strokeCache = null;
+		this.clear();
+	};
+
+	pkg.DrawBoard.prototype = {
+		show: function() {
+			this.outer.style.display = "";
+		},
+
+		close: function() {
+			this.outer.style.display = "none";
+		},
+
+		clear: function() {
+			this.strokes.length = 0;
+			this.currentStroke = null;
+			if (this.strokeCache)
+				this.strokeCache = null;
+			this.updateCache();
+		},
+
+		updateOriginPosition: function () {
+			var pos = $(this.canvas).offset();
+			this.pageOrigin.x = pos.left;
+			this.pageOrigin.y = pos.top;
+		},
+
+		onMouseDown: function(e) {
+			this.updateOriginPosition();
+			var x = e.pageX - this.pageOrigin.x;
+			var y = e.pageY - this.pageOrigin.y;
+			this.dragging = true;
+			this.newStroke(x, y);
+		},
+
+		onMouseLeave: function(e) {
+			this.stopStroke();
+		},
+
+		onMouseMove: function(e) {
+			if (this.dragging) {
+				this.updateOriginPosition();
+				var x = e.pageX - this.pageOrigin.x;
+				var y = e.pageY - this.pageOrigin.y;
+				this.appendPoint(x, y);
+			}
+		},
+
+		onMouseUp: function(e) {
+			this.stopStroke();
+		},
+
+
+		outerElement: function() {
+			return this.outer;
+		},
+
+		newStroke: function(x, y) {
+			this.commitStroke();
+			this.currentStroke = [x, y, 0];
+		},
+
+		stopStroke: function() {
+			this.dragging = false;
+			this.commitStroke();
+		},
+
+		appendPoint: function(x, y) {
+			if (this.currentStroke) {
+				var len = this.currentStroke.length;
+				var dx = x - this.currentStroke[len-3];
+				var dy = y - this.currentStroke[len-2];
+				if ((dx*dx + dy*dy) >= 8) {
+					this.currentStroke.push(x, y, 0);
+					this.updateView();
+				}
+			}
+		},
+
+		commitStroke: function() {
+			if (this.currentStroke) {
+				this.strokes.push(this.currentStroke);
+				this.updateCache();
+				this.currentStroke = null;
+			}
+		},
+
+		updateCache: function() {
+			var g = this.g;
+			this.updateView(true);
+			if (!this.strokeCache) {
+				this.strokeCache = new Image();
+			}
+
+			var _this = this;
+			this.strokeCache.onload = function(){
+				_this.updateView();
+			};
+			this.strokeCache.src = this.canvas.toDataURL();
+		},
+
+		updateView: function(fgonly) {
+			var g = this.g;
+			g.clearRect(0, 0, this.width, this.height);
+			if (!fgonly) {
+				g.fillStyle = "#ddd";
+				g.fillRect(0, this.height >> 1, this.width, 1);
+				g.fillRect(this.width >> 1, 0, 1, this.height);
+			}
+
+			if (this.strokeCache) {
+				g.drawImage(this.strokeCache, 0, 0);
+			}
+
+			if (this.currentStroke) {
+				this.renderStroke(g, this.currentStroke);
+			}
+		},
+
+		renderStroke: function(g, coords, style, transform) {
+			var len = coords.length / 3;
+			if (len < 2)
+				return;
+			var prev_w = 0;
+			this.makeWidth(coords);
+
+			var ox;
+			var oy;
+			var original_x;
+			g.lineCap = 'round';
+			g.lineJoin = 'round';
+			g.strokeStyle = style || "#000";
+			for (i = 0;i < len;i++) {
+				var x = coords[i*3  ];
+				var y = coords[i*3+1];
+
+				if (transform) {
+					original_x = x;
+					x = transform.transformX(x);
+					y = transform.transformY(original_x, y, this.height);
+				}
+
+				var w = coords[i*3+2] * this.lineWidth;
+
+				if (w != prev_w && i) {
+					g.stroke();
+					g.lineWidth = w;
+					g.beginPath();
+					g.moveTo(ox, oy);
+				}
+
+				prev_w = w;
+				if (i) {
+					g.lineTo(x, y);
+				} else {
+					g.beginPath();
+					g.moveTo(x, y);
+				}
+
+				ox = x;
+				oy = y;
+			}
+			g.stroke();
+		},
+
+		makeWidth: function(coords) {
+			var len = coords.length / 3;
+			var i;
+			var ox = coords[0];
+			var oy = coords[1];
+			var all_len = 0;
+
+			if (len > 0) {
+				for (i = 0;i < len;i++) {
+					var dx = coords[i*3  ] - ox;
+					var dy = coords[i*3+1] - oy;
+					ox = coords[i*3  ];
+					oy = coords[i*3+1];
+					all_len += Math.sqrt(dx*dx + dy*dy);
+					coords[i*3+2] = all_len;
+				}
+
+				for (i = 0;i < len;i++) {
+					var u = coords[i*3+2] * 0.05;
+					var v = all_len * 0.05 - u;
+					if (u > 1.0) u = 1.0;
+					if (v > 1.0) v = 1.0;
+					coords[i*3+2] = (u<v) ? u : v;
+				}
+			}
+		},
+
+		fit: function() {
+			var o = this.outer;
+			var h = $(o.parentNode).height() >> 0;
+			o.style.height = h + "px";
+			this.canvas.style.marginTop = (((h-this.height)>>1)-24)+ "px";
+		}
+	};
+
+
+	// =========================== launch ============================
 	pkg.launch_oqunomaker = function(box_id, onComplete) {
 		theApp = new pkg.OqunoMaker(document.getElementById(box_id), 256, onComplete);
 	};
@@ -485,4 +921,11 @@ if (!window.gyuque) { window.gyuque ={}; }
 		if (cls){elm.className = cls;}
 		return elm;
 	};
+
+	function add_css(css) {
+		var s = $H('style');
+		s.type = "text/css";
+		s.appendChild(document.createTextNode(css));
+		$('head')[0].appendChild(s);
+	}
 })(gyuque);
